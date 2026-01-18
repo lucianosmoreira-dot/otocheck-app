5fd5cf6c6f4b93dc50d12f483366798aafa5e1fe
lucianosmoreira-dot <lucianosmoreira@gmail.com>
2026-01-18 18:33:12 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
